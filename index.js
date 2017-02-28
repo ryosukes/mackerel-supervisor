2cf6b7c1144c182ba6e6cb09ef3c9afe8a5a9c53
ryosukes <sasaki@basicinc.jp>
2017-02-28 10:12:53 +0900

    add status

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var shell   = require('shelljs');
 var command = shell.exec('sudo supervisorctl status');
 var status  = command.stdout;
 
-if (status.match(/FATAL/)) {
+if (status.match(/FATAL|STOPPED|EXITED/)) {
 	process.exit(1);
 }
